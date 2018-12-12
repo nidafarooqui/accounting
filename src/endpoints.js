967d184a88070ad41c8d63c98459fb67a25eabc6
Nida <nidafarooqui327@gmail.com>
2018-12-12 16:08:34 +0500

    updated endpoints to the other server

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,7 @@
 
-export const balanceSheet = "http://203.135.63.70:8090/Accounting/services/BalanceSheetWS?wsdl";
-export const cashFlowStatement = "http://203.135.63.70:8090/Accounting/services/CashFlowWS?wsdl";
-export const incomeStatement = "http://203.135.63.70:8090/IncomeStatement/services/GetIncomeStatement?wsdl";
-export const transactionTotals = "http://203.135.63.70:8090/TransactionTotals/services/TransactionTotal?wsdl";
-export const invoiceDetails = "http://203.135.63.70:8090/Accounting/services/InvoiceIdTransactionsWS?wsdl";
-export const invoiceList = "http://203.135.63.70:8090/Accounting/services/InvoiceIdListWS?wsdl";
+export const balanceSheet = "http://203.135.63.69/Accounting/services/BalanceSheetWS?wsdl";
+export const cashFlowStatement = "http://203.135.63.69/Accounting/services/CashFlowWS?wsdl";
+export const incomeStatement = "http://203.135.63.69/IncomeStatement/services/GetIncomeStatement?wsdl";
+export const transactionTotals = "http://203.135.63.69/TransactionTotals/services/TransactionTotal?wsdl";
+export const invoiceDetails = "http://203.135.63.69/Accounting/services/InvoiceIdTransactionsWS?wsdl";
+export const invoiceList = "http://203.135.63.69/Accounting/services/InvoiceIdListWS?wsdl";
